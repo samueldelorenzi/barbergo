b56364ff4364826ec20077d6dabaab5fe65bd6bc
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-10-14 19:34:09 -0300

    tabela so aparece caso tenha algum agendamento

--- views/meus_agendamentos.php
+++ views/meus_agendamentos.php
@@ -1,49 +1,78 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agendamentos do Cliente</title>
-    <link rel="stylesheet" href="style.css"> <!-- Link para o arquivo CSS externo -->
+    <title>Meus agendamentos</title>
+    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="icon" href="../anexos/icone.png" type="image/x-icon">
 </head>
 <body>
-    <div class="container">
-        <h1>Meus Agendamentos</h1>
-        <table class="agendamentos">
-            <thead>
-                <tr>
-                    <th>Serviço</th>
-                    <th>Dia</th>
-                    <th>Hora</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    error_reporting(E_ALL);
-                    ini_set('display_errors', 1);
-                    include "../controllers/banco.php";
-                    include "../helpers/auxiliares.php";
-
-                    if (isset($_SESSION['id_cliente'])) {
-                        $agendamentos = get_agendamentos($conexao, $_SESSION['id_cliente']);
-                        
-                        if (!empty($agendamentos)) {
-                            foreach ($agendamentos as $agendamento) {
-                                echo "<tr>";
-                                echo "<td>" . $agendamento['id_servico'] . "</td>";
-                                echo "<td>" . converte_data_usuario($agendamento['dia']) . "</td>";
-                                echo "<td>" . $agendamento['hora'] . "</td>";
-                                echo "</tr>";
-                            }
-                        } else {
-                            echo "<tr><td colspan='3'>No appointments found.</td></tr>";
-                        }
-                    } else {
-                        echo "<tr><td colspan='3'>Client ID not found in session.</td></tr>";
-                    }
+    <div class="header">
+        <div id="header-left">
+            <a href="../views/form_agendamento.php">Agendar novo horário</a>
+        </div>
+        <div id="header-right">
+            <p>
+                <?php 
+                    echo $_SESSION['usuario_email']; 
                 ?>
-            </tbody>
-        </table>
+            </p>
+            <a href="../controllers/logout.php">Sair</a>
+        </div>
+    </div>
+    <div class="title">
+        <h1>BarberGO</h1>
+        <h3>Meus agendamentos</h3>
+    </div>
+    <div class="container">
+        <?php
+            include "../controllers/banco.php";
+            include "../helpers/auxiliares.php";
+
+            if (isset($_SESSION['id_cliente'])) {
+                $agendamentos = get_agendamentos($conexao, $_SESSION['id_cliente']);
+                
+                if (!empty($agendamentos)) {
+                    echo "<table class='agendamentos'>";
+                    echo    "<thead>";
+                    echo        "<tr>";
+                    echo            "<th>Serviço</th>";
+                    echo            "<th>Dia</th>";
+                    echo            "<th>Hora</th>";
+                    echo            "<th>Preço</th>";                   
+                    echo        "</tr>";
+                    echo    "</thead>";
+                    echo "<tbody>";
+                    foreach ($agendamentos as $agendamento) {
+                        $servico = get_servico_by_id($conexao, $agendamento['id_servico']+1);
+                        $hora_bd = $agendamento['hora'];
+                        $hora_bd = explode(":", $hora_bd);
+                        $hora = $hora_bd[0] . ":" . $hora_bd[1];
+                        echo "<tr>";
+                        echo "<td>" . $servico['nome'] . "</td>";
+                        echo "<td>" . converte_data_usuario($agendamento['dia']) . "</td>";
+                        echo "<td>" . $hora . "</td>";
+                        echo "<td>" . (int)$servico['preco'] . "R$</td>";
+                        echo "</tr>";
+                    }
+                    echo    "</tbody>";
+                    echo "</table>";
+                } 
+                else 
+                {
+                    echo "<a href='../views/form_agendamento.php'>Nenhum horário agendado. Clique aqui para agendar!</a>";
+                }
+            } 
+            else 
+            {
+                echo "Erro ao buscar agendamentos. Tente novamente!";
+            }
+        ?>
     </div>
 </body>
 </html>
