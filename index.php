c2f9219c89531670b6e496e24513b08025284385
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-10-18 01:11:22 -0300

    Criado  as telas de login e cadastro  . vale lembrar que algumas estrutura do projeto também foi alterando no entanto a necessidade de mudar também a main ,

--- index.php
+++ index.php
@@ -1,52 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BarberGO</title>
-    <link rel="stylesheet" href="assets/style.css">
-    <link rel="icon" href="anexos/icone.png" type="image/x-icon">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro - BarberGO</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="shortcut icon" href="anexos/icone.png" type="image/x-icon">
 </head>
-<body>
-    <div class="title">
-        <h1>BarberGO</h1>
-        <h3>Cadastro</h3>
-    </div>
-    <div class="container">
-        <form method="POST" action="controllers/cadastro.php">
-            <fieldset>
-                <label for="nome">Nome:</label>
-                <input type="text" name="nome" placeholder="Nome" required>
+<body class="background-image">
+    <div class="container d-flex align-items-center justify-content-center vh-100">
+        <div class="bg-white rounded col-8 col-lg-4 shadow-lg p-4">
+            <div class="text-center">
+                <img src="assets/img/icone.png" alt="Ícone Barbearia" class="img shadow">
+                <h1 class="fs-4">Crie sua conta e comece a usar o BarberGO</h1>
+            </div>
+            <form method="POST" action="controllers/cadastro.php" class="needs-validation pb-3" novalidate>
+                <div class="input-group mb-3">
+                    <input type="text" name="nome" class="form-control" placeholder="Nome" required>
+                    <label class="input-group-text"><i class="fa-solid fa-user"></i></label>
+                    <div class="invalid-feedback">Preencha o teu nome.</div>
+                    <div class="valid-feedback">Nome válido!</div>
+                </div>
 
-                <label for="sobrenome">Sobrenome:</label>
-                <input type="text" name="sobrenome" placeholder="Sobrenome" required>
+                <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+                    <label class="input-group-text"><i class="fa-solid fa-envelope"></i></label>
+                    <div class="invalid-feedback">Preencha o teu e-mail.</div>
+                    <div class="valid-feedback">E-mail válido!</div>
+                </div>
 
-                <label for="email">E-mail:</label>
-                <input type="email" name="email" placeholder="E-mail" required>
-
-                <label for="senha">Senha:</label>
-                <input type="password" name="senha" placeholder="Senha" required maxlength="16">
+                <div class="input-group mb-3">
+                    <input type="password" name="senha" class="form-control" placeholder="Senha" required maxlength="16">
+                    <label class="input-group-text"><i class="fa-solid fa-lock"></i></label>
+                    <div class="invalid-feedback">Preencha a tua senha.</div>
+                    <div class="valid-feedback">Senha válida!</div>
+                </div>
 
                 <?php
                     session_start();
 
                     if (isset($_SESSION['success_message'])) {
-                        echo '<p style="color: green;">' . $_SESSION['success_message'] . '</p>';
+                        echo '<p class="text-success text-center">' . $_SESSION['success_message'] . '</p>';
                         unset($_SESSION['success_message']);
-                    }
-                    else
-                    {
-                        echo '<p style="color: red;">' . $_SESSION['error_message'] . '</p>';
+                    } elseif (isset($_SESSION['error_message'])) {
+                        echo '<p class="text-danger text-center">' . $_SESSION['error_message'] . '</p>';
                         unset($_SESSION['error_message']);
                     }
                 ?>
 
-                <a href="views/form_login.php" id="link">Já possui cadastro? Clique aqui</a>
-                
-                <input type="submit" name="gravar" value="Cadastrar-se">
-            </fieldset>
-        </f>
+                <div class="text-center">
+                    <a href="views/form_login.php" class="nav-link text-decoration-none">Já possui cadastro? Faça login</a>
+                </div>
 
+                <button type="submit" name="gravar" class="btn btn-primary w-100 mt-3">Cadastrar-se</button>
+            </form>
+        </div>
     </div>
+
+  <!-- Bootstrap JS and dependencies -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/valid.js"></script>
 </body>
 </html>
