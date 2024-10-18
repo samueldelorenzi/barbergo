f03e18a3a9468ab601dc5b9d535dc77c39db9ab3
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-10-18 10:05:14 -0300

    removido session start desnecessario

--- views/form_login.php
+++ views/form_login.php
@@ -76,7 +76,6 @@
 
     <!-- PHP para exibir mensagens -->
     <?php
-        session_start();
         if (isset($_SESSION['success_message'])) {
             echo '<p style="color: green;">' . $_SESSION['success_message'] . '</p>';
             unset($_SESSION['success_message']);
