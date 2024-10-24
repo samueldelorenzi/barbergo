4582d8f1563b604ac0fd5351ab32309a5c3987aa
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-10-24 14:49:47 -0300

    subindo o homepage inicial  sem css

--- index.php
+++ index.php
@@ -16,29 +16,8 @@
         .navbar-brand, .nav-link {
             color: white;
         }
-        .banner {
-            background-image: url('assets/img/banner.jpg');
-            height: 300px;
-            color: white;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-            position: relative;
-        }
-        .banner::after {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background: rgba(0, 0, 0, 0.5);
-        }
-        .banner h1 {
-            font-size: 2.5rem;
-            position: relative;
-        }
+        
+       
         .card-img-top {
             height: 200px;
             object-fit: cover;
