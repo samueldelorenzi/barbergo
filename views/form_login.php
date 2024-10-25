94f0d0d7fadd3a8686da294ba0ccba1682467b64
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-10-25 14:53:42 -0300

    E com enorme prazer que finalizo a Homepage

--- views/form_login.php
+++ views/form_login.php
@@ -18,7 +18,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/Login/style.css">
   <link rel="shortcut icon" href="../assets/img/icone.png" type="image/x-icon">
 </head>
 <body class="background-image">
