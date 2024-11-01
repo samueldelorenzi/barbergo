f4ec318a277a7fa0c67e937538ed9f10fbf1b7ca
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-10-31 22:04:07 -0300

    comentarios breves e remocao de funcoes nao utilizadas

--- controllers/agendamento.php
+++ controllers/agendamento.php
@@ -1,15 +1,20 @@
 <?php 
+// Inicia sessão
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+// Inclui arquivos
 include 'banco.php';
 include '../helpers/Agendamento.php';
 
+
+// Configurações de exibição de erros
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Verifica se o método de requisição é POST e se os dados foram enviados
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data']) && !empty($_POST['data']) && isset($_POST['hora']) && !empty($_POST['hora'])) {
 
     $id_cliente = $_SESSION['id_cliente']; 
@@ -17,18 +22,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data']) && !empty($_P
     $data = mysqli_real_escape_string($conexao, trim($_POST['data']));
     $hora = mysqli_real_escape_string($conexao, trim($_POST['hora']));
     
+    // Cria um novo objeto de agendamento
     $novoAgendamento = new Agendamento($conexao, $id_cliente, $id_servico, $data, $hora);
 
+    // Verifica se o horário está disponível
     if ($novoAgendamento->verificarDisponibilidade()) {
+        // Adiciona o agendamento 
         if ($novoAgendamento->adicionar()) {
             $_SESSION['success_message'] = "Horário marcado com sucesso!";
         } else {
             $_SESSION['error_message'] = "Erro ao marcar horário. Tente novamente.";
         }
+        // Caso o horário não esteja disponível, retorna erro
     } else {
         $_SESSION['error_message'] = "Horário indisponível";
     }
 
+    // Redireciona para a página de agendamento
     header('Location: ../views/form_agendamento.php');
 }
 exit();
