9e9db219479610a8ef93e34d98186a3095507635
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-10-31 21:44:23 -0300

    criadas as bases (nao testei ainda)

--- controllers/agendamento.php
+++ controllers/agendamento.php
@@ -2,30 +2,33 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
 include 'banco.php';
-include '../helpers/auxiliares.php';
+include '../helpers/Agendamento.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data']) && !empty($_POST['data']) && isset($_POST['hora']) && !empty($_POST['hora'])) {
-    echo "Entrou";
-    $agendamento = [
-        'servico' => mysqli_real_escape_string($conexao, trim($_POST['servico'])),
-        'data' => mysqli_real_escape_string($conexao, trim($_POST['data'])),
-        'hora' => mysqli_real_escape_string($conexao, trim($_POST['hora']))
-    ];
 
-    if (verificar_horario($conexao, $agendamento)) {
-        $_SESSION['success_message'] = "Horário marcado!";
-        header('Location: ../views/form_agendamento.php');
-    } 
-    else 
-    {
-        $_SESSION['error_message'] = "Horário indisponível";
-        header('Location: ../views/form_agendamento.php');
-    }
+    $id_cliente = $_SESSION['id_cliente']; 
+    $id_servico = mysqli_real_escape_string($conexao, trim($_POST['servico']));
+    $data = mysqli_real_escape_string($conexao, trim($_POST['data']));
+    $hora = mysqli_real_escape_string($conexao, trim($_POST['hora']));
     
+    $novoAgendamento = new Agendamento($conexao, $id_cliente, $id_servico, $data, $hora);
+
+    if ($novoAgendamento->verificarDisponibilidade()) {
+        if ($novoAgendamento->adicionar()) {
+            $_SESSION['success_message'] = "Horário marcado com sucesso!";
+        } else {
+            $_SESSION['error_message'] = "Erro ao marcar horário. Tente novamente.";
+        }
+    } else {
+        $_SESSION['error_message'] = "Horário indisponível";
+    }
+
+    header('Location: ../views/form_agendamento.php');
 }
 exit();
