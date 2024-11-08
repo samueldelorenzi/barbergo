55a4250752a2566525209e443445a825767c7f26
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2024-11-07 21:54:59 -0300

    adicionado auxiliar que puxa horarios no agendamento, removido auxiliares inutilizados

--- helpers/auxiliares.php
+++ helpers/auxiliares.php
@@ -49,62 +49,6 @@ function get_servico_by_id($conexao, $id_servico)
     }
 }
 
-function adicionar_agendamento($conexao, $agendamento, $id_cliente)
-{
-    $sqlGravar = "INSERT INTO agendamento (id_cliente, id_servico, dia, hora) VALUES (?, ?, ?, ?)";
-    
-    $stmt = mysqli_prepare($conexao, $sqlGravar);
-    
-    if ($stmt) 
-    {
-        mysqli_stmt_bind_param($stmt, "ssss", $id_cliente, $agendamento['servico'], $agendamento['data'], $agendamento['hora']);
-        
-        $result = mysqli_stmt_execute($stmt);
-        
-        mysqli_stmt_close($stmt);
-        
-        return $result;
-    } 
-    else 
-    {
-        return false;
-    }
-}
-function get_agendamentos($conexao, $id_cliente)
-{
-    $sql = "SELECT id_servico, dia, hora FROM agendamento WHERE id_cliente = ? AND CONCAT(dia, ' ', hora) > NOW()";
-    
-    $stmt = mysqli_prepare($conexao, $sql);
-    
-    if ($stmt) 
-    {
-        mysqli_stmt_bind_param($stmt, "s", $id_cliente);
-        
-        mysqli_stmt_execute($stmt);
-        
-        mysqli_stmt_bind_result($stmt, $id_servico, $dia, $hora);
-        
-        $agendamentos = [];
-        
-        while (mysqli_stmt_fetch($stmt)) 
-        {
-            $agendamentos[] = [
-                'id_servico' => $id_servico,
-                'dia' => $dia,
-                'hora' => $hora
-            ];
-        }
-        
-        mysqli_stmt_close($stmt);
-        
-        return $agendamentos;
-    } 
-    else 
-    {
-        return false;
-    }
-}
-
 function converte_data_usuario($data)
 {
     // Separa a data em partes por hífen
@@ -112,4 +56,48 @@ function converte_data_usuario($data)
 
     // Retorna a data no formato dd/mm/aaaa
     return $data[2] . "/" . $data[1] . "/" . $data[0];
+}
+
+function getHorariosDisp($selectedDate)
+{
+    $abertura = 8;
+    $fechamento = 18;
+    $hoje = new DateTime();
+    $horarios_mostrados = 0;
+
+    $isToday = $selectedDate->format('Y-m-d') === $hoje->format('Y-m-d');
+
+    // Define a hora atual para comparação (apenas se for o dia atual)
+    $currentDateTime = new DateTime();
+
+    // Loop para gerar horários
+    for ($hora = $abertura; $hora < $fechamento; $hora++) {
+        for ($minuto = 0; $minuto < 60; $minuto += 30) {
+            // Criar um objeto DateTime para o slot
+            $slotTime = clone $selectedDate;
+            $slotTime->setTime($hora, $minuto); // Definir o horário do slot
+
+            // Mostrar apenas horários futuros se a data for hoje
+            if ($isToday or $selectedDate < $hoje) {
+                if ($slotTime > $currentDateTime) { // Mostrar apenas horários futuros
+                    $horarios_mostrados++;
+                    echo '<div class="horario">';
+                    echo $slotTime->format('H:i'); // Exibir no formato 24h
+                    echo '</div>';
+                }
+            } else {
+                $horarios_mostrados++;
+                echo '<div class="horario">';
+                echo $slotTime->format('H:i'); // Exibir no formato 24h
+                echo '</div>';
+            }
+
+        }
+    }
+    if($horarios_mostrados == 0)
+    {
+        echo '</div>';
+        echo '<br>';
+        echo '<p>Não há horários disponíveis para o dia selecionado.</p>';
+    }
 }
